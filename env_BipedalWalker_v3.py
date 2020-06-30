c74b6af443ed2e59cb8e104a43723d760c57eaad
MD-Lai <marvin.dhlai@gmail.com>
2020-06-30 10:56:33 +1000

    some further cleanup

--- env_BipedalWalker_v3.py
+++ env_BipedalWalker_v3.py
@@ -1,7 +1,7 @@
 import gym
 import helper
 
-env = gym.make("BipedalWalker-v3")
+env = gym.make("BipedalWalker-v2")
 cfg = "cfg_BipedalWalker_v3"
 name = "bipedal"
 
@@ -15,13 +15,14 @@ def eval_genomes(genomes, config):
         observation = env.reset()
         stopped_time = 0 
         for _ in range(800):
+            env.render()
             action = net.activate(observation)
             observation,reward,done,info = env.step(action)
 
             if abs(observation[2]) < 0.001:
                 stopped_time += 1
             if stopped_time >= 50:
-                reward = -80
+                reward = -100
                 done = True
 
             total_reward += reward
